a327a94a29cb1d6d372ae20baaf2f21ad0315620
Lenny Barbe <lenny.barbe@gmail.com>
2023-04-14 11:17:46 +0200

    padding / anti padding

--- projet_IN202/compression.py
+++ projet_IN202/compression.py
@@ -5,6 +5,10 @@ import scipy as sp
 import os
 from math import log10, sqrt
 
+
+'''question 4'''
+
+
 def load(filename):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     toLoad= Image.open(os.path.join(script_dir, filename))
@@ -34,7 +38,6 @@ def RGB_YCbCr(image):
     new_image[:,:,2] = 0.5*image[:,:,0] - 0.4187*image[:,:,1] - 0.0813*image[:,:,2] + 128
     return new_image
 
-
 def YCbCr_RGB(image):
     image = np.float64(image.copy())
     new_image = np.empty(image.shape, dtype = np.uint8)
@@ -43,15 +46,48 @@ def YCbCr_RGB(image):
     new_image[:,:,2] = np.clip(image[:,:,0] + 1.772*(image[:,:,1] - 128), 0, 255)
     return new_image
 
+
+
 def padding(image):
-    new_image = np.empty(image.shape[0],image.shape[1],image.shape[2], dtype = np.uint8)
-    
+    global orig_shape
+    shape = list(image.shape)
+    orig_shape = list(image.shape)
+    shape[0] += 8 - shape[0] % 8 if shape[0] % 8 != 0 else 0
+    shape[1] += 8 - shape[1] % 8 if shape[1] % 8 != 0 else 0
+    new_image = np.empty(shape, dtype = np.uint8)
+    for x in range (orig_shape[0]):
+        for y in range (orig_shape[1]):
+            new_image[x,y] = image[x,y]
+    return new_image
+
+def antipadding(image):
+    global orig_shape
+    new_image = np.empty(orig_shape, dtype = np.uint8)
+    for x in range (orig_shape[0]):
+        for y in range (orig_shape[1]):
+            new_image[x,y] = image[x,y]
     return new_image
 
 
 
+
+
 test = load("test.png")
+
+'''affiche test.png en RGB'''
 # Image.fromarray(test, mode = "RGB").show()
+
+'''transforme test.png en YCbCr'''
 # Image.fromarray(RGB_YCbCr(test), mode = "YCbCr").show()
+
+'''transforme test.png en YCbCr puis de retour en RGB'''
 # Image.fromarray(YCbCr_RGB(RGB_YCbCr(test)), mode = "RGB").show()
-print(psnr(test, YCbCr_RGB(RGB_YCbCr(test))))
+
+'''calcule la ressemblance entre l'image transformée ↑ avec l'image originale'''
+# print(psnr(test, YCbCr_RGB(RGB_YCbCr(test))))
+
+'''transforme l'image pour faire de sorte que les dimensions soient des multiples de 8 (padding)'''
+# Image.fromarray(padding(test), mode = "RGB").show()
+
+'''transforme l'image pour faire de sorte que le padding soit enlevé'''
+# Image.fromarray(antipadding(padding(test)), mode = "RGB").show()
